8ea98a8c65de03db7dd7ab554b0be60294ed71db
Erick Murrieta <ercx80@gmail.com>
2020-05-08 20:17:43 -0400

    Working on HTML pages

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-from flask import Flask, redirect, request, render_template
+from flask import Flask, redirect, request, render_template, url_for
+
+
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
+#Bootstrap(app)
 
-@app.route("/")
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
     return render_template('index.html')
 
-app.run()
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['file']
+        #f.save(secure_filename(f.filename))
+        return 'file uploaded successfully'
+    return render_template('upload.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
