b08c663ca97e64d6bf15447abcd61f9d980c9067
Erick Murrieta <ercx80@gmail.com>
2020-05-25 22:50:52 -0400

    Started working on the add a game plan feature

--- main.py
+++ main.py
@@ -65,6 +65,37 @@ def search():
 
 @app.route('/add', methods=['POST','GET'])
 def add():
+    plan_name = ''
+    formation_choice = ''
+    variation_choice = ''
+
+
+    plan_error = ''
+    formation_error = ''
+    variation_error = ''
+
+    if request.method == 'POST':
+        plan_name = request.form['plan']
+        formation_choice = request.form['choice']
+        variation_choice = request.form['variation']
+        
+        if plan_name == '':
+            plan_error = 'This field cannot be empty'
+        if formation_choice == '':
+            formation_error = 'Please choose a formation'
+        if variation_choice == '':
+            variation_error = 'Please choose a variation'
+        
+        if not plan_error and not formation_error and not variation_error:
+            new_plan = Gameplans(plan_name)
+            new_form = Formations(formation_choice)
+            new_var = Variations(variation_choice)
+
+            db.session.add(new_plan, new_form, new_var)
+            db.session.commit()
+            return redirect('/tally?id={}'.format(new_plan.id, new_form.id, new_var.id))
+        else:
+                return render_template('add.html', plan_error, formation_error, variation_error)
             
     return render_template('add.html')
 
