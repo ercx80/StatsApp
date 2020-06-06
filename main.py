d311ef8597ddc52b0104be6701356aaed6381d7e
Erick Murrieta <ercx80@gmail.com>
2020-06-06 18:52:50 -0400

    still working on forms

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from flask import Flask, redirect, request, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import Form
+from wtforms  import StringField, PasswordField
+
 
 SQLAlchemy_TRACK_MODIFICATIONS=True
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'TheSecret'
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://sql9342668:TKSSHLhfv9@sql9.freemysqlhosting.net/sql9342668'
 app.config['SQLALCHEMY_ECHO']= True
@@ -65,26 +69,27 @@ def search():
 
 @app.route('/add', methods=['POST','GET'])
 def add():
-            
-            
+    if request.method == 'POST':
+        #plan_name = request.form('name')
+        #formation_name = request.form['formation']
+        #variation_name = request.form['variation']
+        plan_name = request.form['name']
+        formation_name = request.form['formation']
+        variation_name = request.form['variation']
+        return redirect('/tally')
+
     return render_template('add.html')
 
-@app.route('/tally', methods=['POST','GET'])
+    
+            
+            
+   
+
+@app.route('/tally', methods=['Get','POST'])
 def tally():
-
-    if request.method == 'POST':
-        plan_name = request.form
-        #formation_choice = request.form
-        #variation_choice = request.form
-        tally = request.form
+      
         
-        
-       
-        return render_template('/tally.html', plan_name=plan_name, tally=tally)
-    #return render_template('tally.html')
-
-#This method is only used to get data from form, not DB.   
-
+    return render_template('tally.html')
 
 
 
